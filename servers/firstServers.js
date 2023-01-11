cfa44faa7b32aa4bf4420c362ccf69d8caeac155
Liseth Moreno <lisethpatriciamorenomarrugo@gmail.com>
2023-01-11 11:30:52 -0500

    implementacion del express en mi aplicativo

--- servers/firstServers.js
+++ servers/firstServers.js
@@ -1,16 +1,38 @@
-const http = require("http");
+// const http = require("http");
 const jsonData = require("../data.json");
 const host = "localhost";
 const port = 9000;
+const fs = require("fs");
+const express = require("express");
+const app = express();
 
-const requestListener = function (req, res) {
-  res.setHeader("Content-Type", "application/json");
-  res.writeHead(200);
+app.get ("/", function (req, res) {
+  res.json(jsonData);
+  res.end();
+})
 
-  res.end(JSON.stringify(jsonData));
-};
+app.post("/", express.json(), function (req, res) {
+  const Data = req.body;
+  console.log(Data);
+  jsonData.push(Data)
+  fs.writeFileSync("../data.json", JSON.stringify(jsonData), function (err) {
+    if (err) throw err;
+    console.log("updated");
+  });
+  res.send("recibido");
+  res.end();
+})
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
+
+// const requestListener = function (req, res) {
+//   res.setHeader("Content-Type", "application/json");
+//   res.writeHead(200);
+//   res.end(JSON.stringify(jsonData));
+// };
+
+// const server = http.createServer(requestListener);
+app.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
+
+module.exports= app;
